379dc3e27aa9839b4a20e75bab2e7a333f5c8bc9
Raman Novakhatski <romabelarus@mail.ru>
2022-02-07 13:34:49 +0300

    Add updateColor func

--- ChangeBG/SettingColorVC.swift
+++ ChangeBG/SettingColorVC.swift
@@ -18,57 +18,73 @@ final class SettingColorVC: UIViewController {
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
     @IBOutlet weak var opacityTextField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setSliders()
+    }
 
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 100
-        redSlider.value = 50
-        
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 100
-        greenSlider.value = 50
-        
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 100
-        blueSlider.value = 50
-        
-        opacitySlider.minimumValue = 0
-        opacitySlider.maximumValue = 100
-        opacitySlider.value = 50
-    }
-    
     @IBAction private func redSliderAction(_ sender: UISlider) {
-        redTextField.text = String(Int(sender.value))
+        redTextField.text = String(Float(sender.value))
+        updateColor()
     }
-    
+
     @IBAction private func greenSliderAction(_ sender: UISlider) {
-        greenTextField.text = String(Int(sender.value))
+        greenTextField.text = String(Float(sender.value))
+        updateColor()
     }
-    
+
     @IBAction private func blueSliderAction(_ sender: UISlider) {
-        blueTextField.text = String(Int(sender.value))
+        blueTextField.text = String(Float(sender.value))
+        updateColor()
     }
-    
+
     @IBAction private func opacitySliderAction(_ sender: UISlider) {
-        opacityTextField.text = String(Int(sender.value))
+        opacityTextField.text = String(Float(sender.value))
+        updateColor()
     }
-    
+
     @IBAction private func redField(_ sender: UITextField) {
     }
-    
+
     @IBAction private func greenField(_ sender: Any) {
     }
-    
+
     @IBAction private func blueField(_ sender: Any) {
     }
-    
+
     @IBAction private func opacityField(_ sender: Any) {
     }
-    
+
     @IBAction private func doneBtn(_ sender: Any) {
     }
+
+    private func updateColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        let opacity = CGFloat(opacitySlider.value)
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: opacity)
+    }
+
+    private func setSliders() {
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.value = 0.5
+
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.value = 0.5
+
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.value = 0.5
+
+        opacitySlider.minimumValue = 0
+        opacitySlider.maximumValue = 1
+        opacitySlider.value = 0.5
+    }
+
     /*
     // MARK: - Navigation
 
